393c1c2cc750ff8d35c3a5075d2a31b478455e6a
modyrefy <modyrefay@gmail.com>
2021-12-30 05:17:33 +0400

    work on localization

--- src/component/languageSwitcher/langSwitcher.tsx
+++ src/component/languageSwitcher/langSwitcher.tsx
@@ -1,4 +1,4 @@
-import React, {  FC } from "react";
+import React, {FC} from "react";
 import deMessages from '../../resources/de.json';
 import enMessages from '../../resources/en.json';
 import esMessages from '../../resources/es.json';
@@ -6,7 +6,6 @@ import frMessages from '../../resources/fr.json';
 import jaMessages from '../../resources/ja.json';
 import zhMessages from '../../resources/zh.json';
 import arMessages from '../../resources/ar.json';
-import {Item} from "../../models/interfaces/item/Item";
 import {LocalStorageGet, LocalStorageSet} from "../../common/localStorage/localStorageHelper";
 
 
@@ -20,19 +19,19 @@ const AllLocalizationResources = {
     ar: arMessages
 };
 
- const LangSwitcher:FC<{locale:string,setlocale:any}>=({locale='de',setlocale})=>{
-    const localeValueFomStorage=LocalStorageGet('lang');
-    const defaultLanguage='en';
-    if(localeValueFomStorage===null||localeValueFomStorage===undefined||localeValueFomStorage==='') {
-        LocalStorageSet('lang',locale);
-    };
+const LangSwitcher: FC<{ locale: string, setlocale: any }> = ({locale = 'de', setlocale}) => {
+    const localeValueFomStorage = LocalStorageGet('lang');
+    if (localeValueFomStorage === null || localeValueFomStorage === undefined || localeValueFomStorage === '') {
+        LocalStorageSet('lang', locale);
+    }
 
-    const handleLanguageSelect = (e:any) => {
+
+    const handleLanguageSelect = (e: any) => {
         //alert(e.target.value)
         setlocale(e.target.value);
-        LocalStorageSet('lang',e.target.value);
+        LocalStorageSet('lang', e.target.value);
     };
-    return(<>
+    return (<>
         <select onChange={handleLanguageSelect} defaultValue={locale}>
             <option value='en'>English</option>
             <option value='es'>French</option>
@@ -44,4 +43,4 @@ const AllLocalizationResources = {
     </>)
 
 }
-export {AllLocalizationResources ,LangSwitcher};
+export {AllLocalizationResources, LangSwitcher};
