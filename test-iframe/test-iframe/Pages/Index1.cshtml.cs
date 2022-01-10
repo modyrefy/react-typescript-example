3947c13455d1a10227b4658c15553591eb0ab7c9
modyrefy <modyrefay@gmail.com>
2022-01-10 22:22:18 +0400

    add test iframe project

---
+++ test-iframe/test-iframe/Pages/Index1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace test_iframe.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
